fe1d4846b0faa20c7a4bde7c42dc779766c94d93
vaibhavborse-dev <vaibhavborse0801@gmail.com>
2026-08-19 14:30:37 +0530

    Create Program_5.c

---
+++ Assignment 1/Program_5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int age;
+	printf("ENter the age ");
+	scanf("%d",&age);
+	
+	if(age >= 18)
+	printf("The person is eligible for voting");
+	
+	else 
+	printf("The person is not eligible for voting");
+}
