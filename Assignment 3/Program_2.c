0d381e19a3e409c0b5a587d83cffbf021c7e2a0e
vaibhavborse-dev <vaibhavborse0801@gmail.com>
2026-08-19 14:40:48 +0530

    Create Program_2.c

---
+++ Assignment 3/Program_2.c
@@ -0,0 +1,13 @@
+// Print table for given number.
+// Input: n = 5
+// Output: 5 10 15 20 25 30 35 40 45 50
+#include<stdio.h>
+void main()
+{
+	int a=1;
+	while(a<=10)
+	{
+		printf("%d\n", a * 5);
+		a++;
+	}
+}
