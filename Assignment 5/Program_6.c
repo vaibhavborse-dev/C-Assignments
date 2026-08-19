e3177999a774a46d9e16a011aff4ed48cb0de848
vaibhavborse-dev <vaibhavborse0801@gmail.com>
2026-08-19 15:03:36 +0530

    Create Program_6.c

---
+++ Assignment 5/Program_6.c
@@ -0,0 +1,16 @@
+// 7. Print a Floyd’s triangle pattern
+// Input: n = 4
+// Output:
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+
+6. Print a half pyramid using numbers
+Input: n = 5
+Output:
+1
+12
+123
+1234
+12345
