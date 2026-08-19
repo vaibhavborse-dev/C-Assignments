138f770e93f98419fea41695bee60feebff931b3
vaibhavborse-dev <vaibhavborse0801@gmail.com>
2026-08-19 14:58:26 +0530

    Update program_1.c

--- Assignment 5/program_1.c
+++ Assignment 5/program_1.c
@@ -1,1 +1,28 @@
+// 1. Print a solid square pattern
+// Input: n = 4
+// Output:
 
+// * * * *
+// * * * *
+// * * * *
+// * * * *
+
+#include<stdio.h>
+void main()
+{
+	
+	int i,j,rows,columns;
+	printf("Enter the no of rows:");
+	scanf("%d",&rows);
+	
+	printf("Enter the no of columns:");
+	scanf("%d",&columns);
+	for(i=1;i<=rows;i++)
+	{
+		for(j=1;j<=columns;j++)
+		{
+			printf("* ");	
+		}
+		printf("\n");
+	}
+}
