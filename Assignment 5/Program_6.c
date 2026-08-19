59610513044b4df7a983501280d85b84c32b7ccc
vaibhavborse-dev <vaibhavborse0801@gmail.com>
2026-08-19 15:05:08 +0530

    Update Program_6.c

--- Assignment 5/Program_6.c
+++ Assignment 5/Program_6.c
@@ -1,16 +1,26 @@
-// 7. Print a Floyd’s triangle pattern
-// Input: n = 4
+// 6. Print a half pyramid using numbers
+// Input: n = 5
 // Output:
 // 1
-// 2 3
-// 4 5 6
-// 7 8 9 10
+// 12
+// 123
+// 1234
+// 12345
 
-6. Print a half pyramid using numbers
-Input: n = 5
-Output:
-1
-12
-123
-1234
-12345
+#include<stdio.h>
+void main()
+{
+	int i,j,rows;
+	printf("Enter the no of rows:");
+	scanf("%d",&rows);
+	
+	for(i=1;i<=rows;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+}
+
