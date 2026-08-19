f1419737293e6c6d86e19c7f0dace7641616d1ef
vaibhavborse-dev <vaibhavborse0801@gmail.com>
2026-08-19 14:36:30 +0530

    Create Program_3.c

---
+++ Assignment 2/Program_3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+	int a=1000,b=200,c=300;
+	
+	if(a>b)
+	{
+		if(a>c)
+		printf("A is greater");
+		else 
+		printf("C is greater");
+	}
+	else if(b>a)
+	{
+	    if(b>c)
+	    printf("B us greater");
+	    else 
+	    printf("C is greater");
+    }
+    
+
+}
