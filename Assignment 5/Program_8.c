65a61343ab7405d78e2f65f09e02301226825706
vaibhavborse-dev <vaibhavborse0801@gmail.com>
2026-08-19 15:06:53 +0530

    Create Program_8.c

---
+++ Assignment 5/Program_8.c
@@ -0,0 +1,38 @@
+// 8. Print a pattern of stars in diamond shape
+// Input: n = 4
+// Output:
+
+// *
+// **
+// ***
+// ****
+// ***
+// **
+// *
+
+void main()
+{
+	int i,j,row;
+	
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("* ");
+		}ddd
+		printf("\n");
+	}
+	for(i=4;i>=1;i--)
+	{
+		for(j=1;j<=i;j++) 
+		{
+		
+		printf("* ");
+	}
+  		printf("\n");
+
+	}
+	
+}
+
+
