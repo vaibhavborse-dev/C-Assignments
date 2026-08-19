e461263073e6101f7992ca786a24bf10a7e0a6c7
vaibhavborse-dev <vaibhavborse0801@gmail.com>
2026-08-19 14:28:16 +0530

    Create Program_1.c

---
+++ Assignment 1/Program_1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int num;
+	printf("Enter the number");
+	scanf("%d",&num);
+	
+	if(num%2==0)
+	printf("Number is even ");
+	
+	else 
+	{
+	printf("number is odd");
+	}
+}
