789a739dfba19dd419b10d9accec956c0ae9e5b1
vaibhavborse-dev <vaibhavborse0801@gmail.com>
2026-08-31 11:16:52 +0530

    Create Program_8.c

---
+++ Assignment 8/Program_8.c
@@ -0,0 +1,22 @@
+void main()
+{
+	int arr[5]= {1,2,3,43,5};
+		int brr[5]={10,20,30,40,50};
+
+    
+    int crr[10],i;
+    
+    for(i=0;i<5;i++)
+    {
+    	crr[i]=arr[i];
+	}
+	
+
+	for(i=5;i<10;i++)
+	{
+		crr[i]=brr[i-5];
+	}
+	
+	for(i=0;i<10;i++)
+		printf("%d,",crr[i]);
+}
