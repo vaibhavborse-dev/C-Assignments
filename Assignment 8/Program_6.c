4f29de06bfef94e67dd3efa8af355d623307ebe4
vaibhavborse-dev <vaibhavborse0801@gmail.com>
2026-08-31 10:55:32 +0530

    Create Program_6.c

---
+++ Assignment 8/Program_6.c
@@ -0,0 +1,33 @@
+void main()
+{
+	int arr[]={7,27,65,34,2,65};
+	
+	int i,j,count;
+	
+	for(i=0;i<6;i++)
+	{
+		count=0;
+		j=2;
+		while(j<arr[i])
+		{
+			if(arr[i]%j == 0)
+			{
+				count=1;
+				break;
+			}
+			j++;
+		}
+			
+	
+	if(count==0)
+	{
+		printf("%d is prime \n",arr[i]);
+		
+	}
+	else 
+	{
+	
+		printf("%d is not prime \n",arr[i]);
+	}
+}
+}
