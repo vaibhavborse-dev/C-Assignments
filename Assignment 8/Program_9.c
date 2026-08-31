6d84781dc6921b0f2399746f90aee7f2c80f1116
vaibhavborse-dev <vaibhavborse0801@gmail.com>
2026-08-31 11:27:32 +0530

    Create Program_9.c

---
+++ Assignment 8/Program_9.c
@@ -0,0 +1,18 @@
+void main()
+{
+	int arr[]={65,76,44,34,23,34};
+	
+	int brr[6];
+	
+	int j=0;
+	for(int i=5;i>=0;i--)
+	{
+	  	brr[j]=arr[i];
+	  	j++;
+	}
+	
+	for(int i=0;i<6;i++)
+	{
+		printf("%d,",brr[i]);
+	}
+}
