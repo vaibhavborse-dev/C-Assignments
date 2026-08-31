87b0c313dee5bd94b25141f78fd35fc5c3e5a9d1
vaibhavborse-dev <vaibhavborse0801@gmail.com>
2026-08-31 11:42:17 +0530

    Create Program_10.c

---
+++ Assignment 8/Program_10.c
@@ -0,0 +1,23 @@
+void main()
+{
+	int arr[]={54,65,23,98,22,31,11};
+	
+	int i,temp;
+	for(i=0;i<7;i++)
+	{
+		for(int j=i+1;j<7;j++)
+		{
+			if(arr[i]>arr[j])
+			{
+				temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+			}
+		}
+		
+	}
+	for(i=0;i<7;i++)
+	{
+		printf("%d,",arr[i]);
+	}
+}
