d6e86b9d97ecf4478e91486d980d8d475b71f19e
vaibhavborse-dev <vaibhavborse0801@gmail.com>
2026-09-19 14:51:03 +0530

    Create program 10_1.c

---
+++ Assignment 14/program 10_1.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+struct product
+{
+	int id;
+	char name[20];
+	int quantity;
+	int price;
+};
+
+struct product store()
+{
+	struct product p;
+	
+	printf("Enter the product id:");
+	scanf("%d",&p.id);
+	
+	printf("Enter the product name:");
+	scanf("%s",p.name);
+	
+	printf("Enter the quantity:");
+	scanf("%d",&p.quantity);
+	
+	printf("Enter the price:");
+	scanf("%d",&p.price);
+	
+	return p;
+}
+
+void display(struct product p)
+{
+	printf("Product id is %d\n",p.id);
+	printf("Product name is %s\n",p.name);
+	printf("Product quantity is %d\n",p.quantity);
+	printf("Product price is %d\n",p.price);
+}
+
+void main()
+{
+	struct product p;
+	
+	p=store();
+	display(p);
+}
